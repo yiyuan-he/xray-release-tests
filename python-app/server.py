d598eab766d21d9d9a0fece9e35c2e484f1982ab
yiyuanh <yiyuanh@amazon.com>
2025-01-21 15:55:06 -0800

    update python sample app

--- python-app/server.py
+++ python-app/server.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 import boto3
 from aws_xray_sdk.core import xray_recorder, patch
+from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 app = Flask(__name__)
 
@@ -11,33 +12,31 @@ xray_recorder.configure(
     sampling=True,
     context_missing='LOG_ERROR',
     daemon_address='127.0.0.1:2000',
-    service="PythonXRayTestAppAutomatic"
+    service="PythonXRayTestApp"
 )
+XRayMiddleware(app, xray_recorder)
 
 s3_client = boto3.client('s3')
 
 @app.route('/generate-automatic-traces', methods=['GET'])
 def automatic_trace():
-    with xray_recorder.in_segment('AutomaticTraceHandler') as segment:
-        response = s3_client.list_buckets()
-        bucket_names = [bucket['Name'] for bucket in response.get('Buckets', [])]
-        return jsonify(bucket_names)
+    response = s3_client.list_buckets()
+    bucket_names = [bucket['Name'] for bucket in response.get('Buckets', [])]
+    return jsonify(bucket_names)
 
 @app.route('/generate-manual-traces', methods=['GET'])
 def manual_trace():
-    # Start a root segment for the HTTP requesst
-    with xray_recorder.in_segment('ManualTraceHandler') as segment:
-        # Start a subsegment for the S3 API call
-        with xray_recorder.in_subsegment('MockOperation1'):
-            print("Simulating mock operation 1")
-            # Start a nested subsegment for extracting bucket names
-            with xray_recorder.in_subsegment('ProcessMockData'):
-                mock_buckets = ['mock-bucket1', 'mock-bucket2', 'mock-bucket3']
-                # Add metadata to the subsegment
-                segment.put_annotation('first_bucket_name', mock_buckets[0])
-        # Make a second S3 API call and instrument with a sibling subsegment
-        with xray_recorder.in_subsegment('MockOperation2'):
-            print("Simulating mock operation 2")
+    # Start a subsegment for the S3 API call
+    with xray_recorder.in_subsegment('MockOperation1') as subsegment:
+        print("Simulating mock operation 1")
+        # Start a nested subsegment for extracting bucket names
+        with xray_recorder.in_subsegment('ProcessMockData'):
+            mock_buckets = ['mock-bucket1', 'mock-bucket2', 'mock-bucket3']
+            # Add metadata to the subsegment
+            subsegment.put_annotation('first_bucket_name', mock_buckets[0])
+    # Make a second S3 API call and instrument with a sibling subsegment
+    with xray_recorder.in_subsegment('MockOperation2'):
+        print("Simulating mock operation 2")
     return jsonify(mock_buckets)
 
 if __name__ == '__main__':
