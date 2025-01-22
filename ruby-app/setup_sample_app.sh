52437442b3576ede5a0c550406c930a91ea6115d
yiyuanh <yiyuanh@amazon.com>
2025-01-22 12:35:35 -0800

    make ruby setup script idempotent

--- ruby-app/setup_sample_app.sh
+++ ruby-app/setup_sample_app.sh
@@ -2,6 +2,7 @@
 
 # Define variable
 GEMFILE="Gemfile"
+GEMFILE_CONFIGURATION="Gemfile_configuration"
 AWS_SDK_S3_LINE="gem 'aws-sdk-s3'"
 
 # Prompt the user for the X-Ray SDK commit hash
@@ -11,6 +12,10 @@ done
 
 # Use the specified commit for our X-Ray SDK dependency
 AWS_XRAY_SDK_LINE="gem 'aws-xray-sdk', git: 'https://github.com/aws/aws-xray-sdk-ruby.git', ref: '$COMMIT_HASH', require: ['aws-xray-sdk/facets/rails/railtie']"
+
+# Overwrite the Gemfile with the base contents
+echo "Resetting the Gemfile to the base configuration..."
+cp "$GEMFILE_CONFIGURATION" "$GEMFILE"
 
 echo "Adding dependencies to the Gemfile..."
 {
